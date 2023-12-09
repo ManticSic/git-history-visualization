ddb1a01bfc3f0dd188875e92d1af474be32a8192
Darius Weber <darius.weber@posteo.de>
2023-12-10 00:39:30 +0100

    Add util class for commits (must be replaced)

---
+++ gitHistoryVisualization/CommitUtil.cs
@@ -0,0 +1,39 @@
+﻿using LibGit2Sharp;
+
+
+namespace gitHistoryVisualization;
+
+
+public class CommitUtil
+{
+    public Commit GetRootCommit(Repository repo)
+    {
+        Commit commit = repo.Head.Tip;
+
+        while (commit.Parents.Any())
+        {
+            commit = commit.Parents.First();
+        }
+
+        return commit;
+    }
+
+    public CommitSummary CommitToCommitSummary(Commit commit, Repository repository)
+    {
+        Tree  commitTree       = commit.Tree;
+        Tree? parentCommitTree = null;
+
+        bool isRootCommit = true;
+
+        // handle root commit
+        if (commit.Parents.Any())
+        {
+            isRootCommit     = false;
+            parentCommitTree = commit.Parents.First().Tree;
+        }
+
+        Patch patch = repository.Diff.Compare<Patch>(parentCommitTree, commitTree);
+
+        return new CommitSummary(commit.Author.When, patch.LinesAdded, patch.LinesDeleted, isRootCommit);
+    }
+}
