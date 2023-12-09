198fd17d5a2871f6566a0e8c705fff6eb0faecb2
Darius Weber <darius.weber@posteo.de>
2023-12-10 00:40:16 +0100

    Add class to group commits of the same day

---
+++ gitHistoryVisualization/DateSummary.cs
@@ -0,0 +1,45 @@
+﻿namespace gitHistoryVisualization;
+
+
+public struct DateSummary
+{
+    private readonly IEnumerable<CommitSummary> _commits;
+    private readonly Lazy<int>                  _lazyLinesAdded;
+    private readonly Lazy<int>                  _lazyLinesRemoved;
+
+    public DateSummary(DateTime dateTime, IEnumerable<CommitSummary> sourceCommits, bool containsRootCommit)
+    {
+        When               = dateTime;
+        ContainsRootCommit = containsRootCommit;
+
+        _commits          = sourceCommits;
+        _lazyLinesAdded   = CreateLazyLinesAdded();
+        _lazyLinesRemoved = CreateLazyLinesRemoved();
+    }
+
+    public DateTime When               { get; }
+    public bool     ContainsRootCommit { get; }
+    public int      LinesAdded         => _lazyLinesAdded.Value;
+    public int      LinesRemoved       => _lazyLinesRemoved.Value;
+
+
+    private Lazy<int> CreateLazyLinesAdded()
+    {
+        return new Lazy<int>(LinesAddedFactory);
+    }
+
+    private Lazy<int> CreateLazyLinesRemoved()
+    {
+        return new Lazy<int>(LinesRemovedFactory);
+    }
+
+    private int LinesAddedFactory()
+    {
+        return _commits.Sum(commit => commit.LinesAdded);
+    }
+
+    private int LinesRemovedFactory()
+    {
+        return _commits.Sum(commit => commit.LinesRemoved);
+    }
+}
