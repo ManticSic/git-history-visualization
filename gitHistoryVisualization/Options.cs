f08fa070dd6cd57ab00913611e88ea278d28c195
Darius Weber <darius.weber@posteo.de>
2023-12-10 00:43:29 +0100

    Add option object for CommandLineParser (temporary)

---
+++ gitHistoryVisualization/Options.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+
+namespace gitHistoryVisualization;
+
+
+public class Options
+{
+    [Option('i', "input", Required = true, HelpText = "Path to git repository.")]
+    public string PathToRepository { get; set; }
+
+    [Option('o', "output", Required =true, HelpText = "Path to save the png file.")]
+    public string OutputPath { get; set; }
+
+    [Option('s', "size", Required = false, Default = DrawUtil.RefCanvasSize, HelpText = "Size of the image (square).")]
+    public int Size { get; set; }
+}
