d6b14b467398c09a5e4b907d905a01cd2b83a41e
Darius Weber <darius.weber@posteo.de>
2023-12-10 00:35:09 +0100

    Add object to group all relevant data

---
+++ gitHistoryVisualization/CommitSummary.cs
@@ -0,0 +1,18 @@
+﻿namespace gitHistoryVisualization;
+
+
+public struct CommitSummary
+{
+    public CommitSummary(DateTimeOffset when, int linesAdded, int linesRemoved, bool isRootCommit)
+    {
+        When         = when.Date;
+        LinesAdded   = linesAdded;
+        LinesRemoved = linesRemoved;
+        IsRootCommit = isRootCommit;
+    }
+
+    public DateTime When         { get; }
+    public int      LinesAdded   { get; }
+    public int      LinesRemoved { get; }
+    public bool     IsRootCommit { get; }
+}
